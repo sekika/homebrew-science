63c7b56a534e8d0264e5ab38d6318921aef033b8
Katsutoshi Seki <sekika@users.noreply.github.com>
2016-09-10 19:33:14 +0900

    swrcfit 3.0
    swrcfit 3.0

--- swrcfit.rb
+++ swrcfit.rb
@@ -2,24 +2,25 @@ class Swrcfit < Formula
   desc "Fitting of soil water retention curve"
   homepage "http://swrcfit.sourceforge.net/"
   # doi "10.5194/hessd-4-407-2007"
-  url "https://github.com/sekika/swrcfit/archive/v2.1.tar.gz"
-  sha256 "a6038544e58db6ae7e7c310cd6537ca4509a059035667834f4b6105e0b76c1d8"
+  url "https://github.com/sekika/swrcfit/archive/v3.0.tar.gz"
+  sha256 "bee47347bad5db0ac72597b82d5fff20278e57d5792e431f6d987c52360d7021"
 
   head "https://github.com/sekika/swrcfit.git"
 
   bottle do
     cellar :any_skip_relocation
-    sha256 "bf912ac45ab874a4a3a9127ec019d952429acb84764aee9395fd7447b2ac8e76" => :el_capitan
-    sha256 "24b0d41cf132bd5d095aa3f7db1eb1fcd9f248bb7d016dade491eb81d1c7e6a3" => :yosemite
-    sha256 "8e57d8f35d20d8cc12e4218b1dce690230ccf45d1db0477ef6639eeae0856894" => :mavericks
+    revision 1
+    sha256 "77219e3af8a12a5a6e8bda365f59149e1c40f1c8fca5fa390d8bf9361a48b580" => :el_capitan
+    sha256 "8876931c4ca1a5b7a372f2570719f9374cc9ac9b0eeac1851ee85a01620846a0" => :yosemite
+    sha256 "d49f6726358d66d28af8a4ac4addb15ae2e5a331e2dfb54112ca7083a4ee994e" => :mavericks
   end
 
   depends_on "octave"
   depends_on "wget"
 
   def install
-    (share / "swrcfit/example").install "swrc.txt"
-    system "./Install.sh", bin
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "install"
   end
 
   test do
